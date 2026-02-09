0430e4595117c3f6460dcd7ffb972d9c629b78ff
YetonDew <ariasjefferson@hotmail.com>
2026-02-09 20:52:01 +0100

    x

--- push_swap.h
+++ push_swap.h
@@ -4,13 +4,24 @@
 # include <stddef.h>
 # include <stdint.h>
 # include <stdlib.h>
+# include <stdbool.h>
+# include <limits.h>
 # include <unistd.h>
 
-typedef struct s_list
+int swap(t_stack **a);
+
+typedef struct s_node
 {
+	int				nbr;
+	int				index;
+	int				push_cost;
+	bool			above_median;
+	bool			cheapest;
 	void			*content;
-	struct s_list	*next;
-}					t_list;
+	struct s_node	*target_node;
+	struct s_node	*prev;
+	struct s_node	*next;
+}					t_stack;
 
 
 #endif
