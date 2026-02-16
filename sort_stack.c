a74e2940c471b312847e5dea76ff380822d0f5d9
YetonDew <ariasjefferson@hotmail.com>
2026-02-16 13:36:36 +0100

    Add sort_stack.c: main Turkish algorithm sorting loop with min_on_top

---
+++ sort_stack.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_stack.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajeffers <ajeffers@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/16 14:00:00 by ajeffers          #+#    #+#             */
+/*   Updated: 2026/02/16 13:10:40 by ajeffers         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	rotate_both(t_stack **a, t_stack **b, t_stack *cheapest)
+{
+	while (*b != cheapest->target_node && *a != cheapest)
+		rr(a, b, true);
+	current_index(*a);
+	current_index(*b);
+}
+
+static void	rev_rotate_both(t_stack **a, t_stack **b, t_stack *cheapest)
+{
+	while (*b != cheapest->target_node && *a != cheapest)
+		rrr(a, b, true);
+	current_index(*a);
+	current_index(*b);
+}
+
+static void	move_a_to_b(t_stack **a, t_stack **b)
+{
+	t_stack	*cheapest;
+
+	cheapest = get_cheapest(*a);
+	if (cheapest->above_median && cheapest->target_node->above_median)
+		rotate_both(a, b, cheapest);
+	else if (!(cheapest->above_median)
+		&& !(cheapest->target_node->above_median))
+		rev_rotate_both(a, b, cheapest);
+	prep_for_push(a, cheapest, 'a');
+	prep_for_push(b, cheapest->target_node, 'b');
+	pb(a, b);
+}
+
+static void	move_b_to_a(t_stack **a, t_stack **b)
+{
+	prep_for_push(a, (*b)->target_node, 'a');
+	pa(a, b);
+}
+
+void	sort_stack(t_stack **a, t_stack **b)
+{
+	int	len_a;
+
+	len_a = stack_len(*a);
+	if (len_a-- > 3 && !is_sorted(*a))
+		pb(a, b);
+	if (len_a-- > 3 && !is_sorted(*a))
+		pb(a, b);
+	while (len_a-- > 3 && !is_sorted(*a))
+	{
+		init_nodes_a(*a, *b);
+		move_a_to_b(a, b);
+	}
+	sort_three(a);
+	while (*b)
+	{
+		init_nodes_b(*a, *b);
+		move_b_to_a(a, b);
+	}
+	current_index(*a);
+	min_on_top(a);
+}
+
+void	min_on_top(t_stack **a)
+{
+	t_stack	*min;
+
+	min = find_min(*a);
+	while (*a != min)
+	{
+		if (min->above_median)
+			ra(a, true);
+		else
+			rra(a, true);
+	}
+}
