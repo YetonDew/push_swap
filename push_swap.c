f639c5fd1a420a5a4fd4d8ae5c2c4bc9db3afee6
YetonDew <ariasjefferson@hotmail.com>
2026-02-16 13:36:52 +0100

    Add push_swap.c: main function with argument parsing and sort dispatch

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,57 @@
 /*   By: ajeffers <ajeffers@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 17:55:09 by ajeffers          #+#    #+#             */
-/*   Updated: 2026/02/09 17:55:11 by ajeffers         ###   ########.fr       */
+/*   Updated: 2026/02/16 13:10:40 by ajeffers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	free_split(char **split)
+{
+	int	i;
+
+	if (!split)
+		return ;
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
+
+int	main(int argc, char **argv)
+{
+	t_stack	*a;
+	t_stack	*b;
+	char	**args;
+
+	a = NULL;
+	b = NULL;
+	if (argc == 1 || (argc == 2 && !argv[1][0]))
+		return (1);
+	if (argc == 2)
+	{
+		args = ft_split(argv[1], ' ');
+		if (!args)
+			return (1);
+		init_stack_a(&a, args);
+		free_split(args);
+	}
+	else
+		init_stack_a(&a, argv + 1);
+	if (!is_sorted(a))
+	{
+		if (stack_len(a) == 2)
+			sa(&a);
+		else if (stack_len(a) == 3)
+			sort_three(&a);
+		else
+			sort_stack(&a, &b);
+	}
+	free_stack(&a);
+	return (0);
+}
 
