2e3ca5c894ee056723f012b6d5a228b8bae71a5e
YetonDew <ariasjefferson@hotmail.com>
2026-02-16 13:32:35 +0100

    Add stack_utils.c: stack_len, find_last, find_min, find_max, is_sorted

---
+++ stack_utils.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajeffers <ajeffers@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/16 14:00:00 by ajeffers          #+#    #+#             */
+/*   Updated: 2026/02/16 13:10:40 by ajeffers         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	stack_len(t_stack *stack)
+{
+	int	count;
+
+	count = 0;
+	while (stack)
+	{
+		count++;
+		stack = stack->next;
+	}
+	return (count);
+}
+
+t_stack	*find_last(t_stack *stack)
+{
+	if (!stack)
+		return (NULL);
+	while (stack->next)
+		stack = stack->next;
+	return (stack);
+}
+
+t_stack	*find_min(t_stack *stack)
+{
+	t_stack	*min;
+
+	if (!stack)
+		return (NULL);
+	min = stack;
+	while (stack)
+	{
+		if (stack->nbr < min->nbr)
+			min = stack;
+		stack = stack->next;
+	}
+	return (min);
+}
+
+t_stack	*find_max(t_stack *stack)
+{
+	t_stack	*max;
+
+	if (!stack)
+		return (NULL);
+	max = stack;
+	while (stack)
+	{
+		if (stack->nbr > max->nbr)
+			max = stack;
+		stack = stack->next;
+	}
+	return (max);
+}
+
+bool	is_sorted(t_stack *stack)
+{
+	if (!stack)
+		return (true);
+	while (stack->next)
+	{
+		if (stack->nbr > stack->next->nbr)
+			return (false);
+		stack = stack->next;
+	}
+	return (true);
+}
